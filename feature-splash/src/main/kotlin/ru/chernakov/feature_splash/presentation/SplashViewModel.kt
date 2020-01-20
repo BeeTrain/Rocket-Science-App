bc169fcb02ca3c38b3649ec67cf7ed30b04c60f1
Chernakov M.E <maksim.chernakov@e-legion.com>
2020-01-20 15:35:10 +0300

    00036 - fix checkstyle errors

--- feature-splash/src/main/kotlin/ru/chernakov/feature_splash/presentation/SplashViewModel.kt
+++ feature-splash/src/main/kotlin/ru/chernakov/feature_splash/presentation/SplashViewModel.kt
@@ -1,8 +1,6 @@
 package ru.chernakov.feature_splash.presentation
 
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.delay
-import ru.chernakov.core_base.util.lifecycle.SingleLiveEvent
 import ru.chernakov.core_ui.presentation.viewmodel.BaseViewModel
 
 class SplashViewModel(private val firebaseAuth: FirebaseAuth) : BaseViewModel() {
