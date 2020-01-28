5872438c6cf896bdb43557af803059ab492f2051
Chernakov M.E <maksim.chernakov@e-legion.com>
2020-01-28 11:55:12 +0300

    00051 - update libraries versions

--- buildSrc/src/main/kotlin/ru/chernakov/rocketscienceapp/Versions.kt
+++ buildSrc/src/main/kotlin/ru/chernakov/rocketscienceapp/Versions.kt
@@ -16,15 +16,15 @@ object Versions {
     const val checkstyle = "8.28"
     const val jacocoTools = "0.7.6.201602180812"
 
-    const val kotlinCoroutines = "1.3.1"
+    const val kotlinCoroutines = "1.3.3"
     const val koin = "2.1.0-alpha-10"
     const val multidex = "2.0.1"
 
     const val support = "1.0.0"
     const val appCompat = "1.2.0-alpha01"
     const val constraint = "2.0.0-beta4"
-    const val material = "1.2.0-alpha03"
-    const val navigation = "2.1.0"
+    const val material = "1.2.0-alpha04"
+    const val navigation = "2.2.0"
 
     const val firebaseDatabase = "19.2.0"
     const val firebaseAuth = "19.2.0"
@@ -36,13 +36,13 @@ object Versions {
     const val ktx = "1.2.0-rc01"
 
     const val fresco = "1.3.0"
-    const val archComponents = "2.2.0-rc03"
+    const val archComponents = "2.2.0"
 
     const val playServices = "11.0.2"
     const val crashlytics = "2.10.1"
     const val stetho = "1.5.0"
 
-    const val okHttp = "4.3.0"
+    const val okHttp = "4.3.1"
     const val retrofit = "2.7.1"
     const val timber = "4.7.1"
     const val leakCanary = "2.0-alpha-3"
