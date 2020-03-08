fef171396f4c92679c069e89504d44a3bc58ac0e
Max Chernakov <41rm4kc@gmail.com>
2020-03-08 12:52:29 +0300

    00100 - update libraries versions

--- buildSrc/src/main/kotlin/ru/chernakov/rocketscienceapp/Versions.kt
+++ buildSrc/src/main/kotlin/ru/chernakov/rocketscienceapp/Versions.kt
@@ -10,22 +10,22 @@ object Versions {
     const val firebaseCrashlyticsPlugin = "2.0.0-beta01"
     const val googleServices = "4.3.3"
     const val gradleVersions = "0.27.0"
-    const val kotlin = "1.3.61"
+    const val kotlin = "1.3.70"
 
     const val ktLint = "0.36.0"
-    const val detekt = "1.5.0"
+    const val detekt = "1.7.0-beta1"
     const val checkstyle = "8.29"
     const val jacocoTools = "0.7.6.201602180812"
 
-    const val kotlinCoroutines = "1.3.3"
-    const val koin = "2.1.0-alpha-10"
+    const val kotlinCoroutines = "1.3.4"
+    const val koin = "2.1.3"
     const val multidex = "2.0.1"
 
     const val support = "1.0.0"
-    const val appCompat = "1.2.0-alpha02"
+    const val appCompat = "1.2.0-alpha03"
     const val constraint = "2.0.0-beta4"
-    const val material = "1.2.0-alpha04"
-    const val navigation = "2.2.0"
+    const val material = "1.2.0-alpha05"
+    const val navigation = "2.3.0-alpha03"
 
     const val firebaseDatabase = "19.2.0"
     const val firebaseAuth = "19.2.0"
@@ -35,7 +35,7 @@ object Versions {
     const val firebaseUiAuth = "4.3.1"
     const val gmsAuth = "17.0.0"
 
-    const val ktx = "1.3.0-alpha01"
+    const val ktx = "1.3.0-alpha02"
 
     const val fresco = "1.3.0"
     const val archComponents = "2.2.0"
@@ -44,7 +44,7 @@ object Versions {
     const val crashlytics = "2.10.1"
     const val stetho = "1.5.0"
 
-    const val okHttp = "4.3.1"
+    const val okHttp = "4.4.0"
     const val retrofit = "2.7.1"
     const val timber = "4.7.1"
     const val leakCanary = "2.0-alpha-3"
@@ -52,5 +52,5 @@ object Versions {
     const val pagination = "2.1.1"
 
     const val junit = "4.13"
-    const val mockito = "3.2.4"
+    const val mockito = "3.3.1"
 }
